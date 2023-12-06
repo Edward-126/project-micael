65f22e0e87521cba49f4cb2cf259c136c408f679
T-126 <rathnayaka3548@gmail.com>
2023-12-06 20:53:02 +0530

    .env added

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,15 +2,13 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: "19i2868n",
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2023-12-06",
   useCdn: false,
-  token:
-    "skKPw3b5DcEV7PU9kTol1qfyRdk6WJeoTu1NE5nA5gjx1C4hWtli6T848MwAHZ6JOmB7788XZZivhRP1t3CBAtgbDtxT0P47U3rTDg6cXN5iGGKHueDenvQZfQjtAxAgjlGUYFOm4MHZzSFOjwZA8F2AdkZcu5jBv4yrQHSiM2JR1r9Kyg8c",
+  token: import.meta.env.VITE_SANITY_TOKEN,
+  ignoreBrowserTokenWarning: true,
 });
-
-// ! FIX THIS SHIT!
 
 const builder = imageUrlBuilder(client);
 
